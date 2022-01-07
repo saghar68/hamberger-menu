969a29cc0eed1cd3fc28030c9ecaa70339a27226
saghi <saghi.abazari@gmail.com>
2022-01-07 16:07:28 +0000

    Add all files.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Berger from './Berger';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Berger />
+    );
+  }
+}
+
+export default App;
+
